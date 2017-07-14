4019361a741005eeab6c4f82b9af92e22dc45aab
amoshyc <amoshuangyc@gmail.com>
2017-07-14 21:39:17 +0800

    Update at 2017-07-14 21-39-17

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ from keras.callbacks import Callback, ModelCheckpoint, CSVLogger
 
 def get_callbacks(name):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_path = Path('log/{} ({}).csv'.format(name, now))
-    weight_path = Path('/tmp/{}_{epoch:02d}_{val_binary_accuracy:.3f}.h5'.format(name))
+    log_path = 'log/{} ({}).csv'.format(name, now)
+    weight_path = '/tmp/{}_{epoch:02d}_{val_binary_accuracy:.3f}.h5'.format(name)
 
     return [
         CSVLogger(log_path),
