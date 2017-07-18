3058af32c5e51ca8ef84a9e74023aaa4580cb931
amoshyc <amoshuangyc@gmail.com>
2017-07-18 17:14:54 +0800

    Update at 2017-07-18 17-14-54

--- explore.py
+++ explore.py
@@ -17,7 +17,7 @@ def get_video_info(video_dir):
 
 if __name__ == '__main__':
     dataset = Path('~/dataset')
-    all_info = [get_video_info(x) for x in dataset.iter_dir() if x.is_dir()]
+    all_info = [get_video_info(x) for x in dataset.iterdir() if x.is_dir()]
     total_len = sum(info['len'] for info in all_info)
     total_hl = sum(info['hl'] for info in all_info)
     print('total len', total_len)
