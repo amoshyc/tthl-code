5a9f0a08ceed1d78d40d259067723534649f0ef0
amoshyc <amoshuangyc@gmail.com>
2017-07-18 22:29:09 +0800

    Update at 2017-07-18 22-29-09

--- test.py
+++ test.py
@@ -7,22 +7,22 @@ from moviepy.editor import VideoFileClip
 
 def window_generator(video_dirs, n_samples, batch_size, timesteps):
     videos = [VideoFileClip(str(x / 'video.mp4')) for x in video_dirs]
+    labels = [
+        json.load((x / 'label.json').open())['label'] for x in video_dirs
+    ]
 
     windows = []
-    for video_id, video in enumerate(video):
+    for video_id, (video, label) in enumerate(zip(videos, labels)):
         fps, dur = video.fps, video.duration
         n_frames = round(dur * fps)
-        start = (e - timesteps) / fps
-        end = (e) / fps
-        label = json.load((x / 'label.json').open())['label']
-        cur_windows = [(video_id, start, end, label[e - 1])
-                       for e in range(timsteps, n_frames)]
-        window.extend(cur_windows)
+        cur_windows = [(video_id, (e - timesteps) / fps, (e) / fps, label[e - 1])
+                       for e in range(timesteps, n_frames)]
+        windows.extend(cur_windows)
 
     windows = random.sample(windows, k=n_samples)
 
     idx = 0
-    x_batch = np.zeros((batch_size, timestep, 224, 224, 3), dtype=np.float32)
+    x_batch = np.zeros((batch_size, timesteps, 224, 224, 3), dtype=np.float32)
     y_batch = np.zeros((batch_size, 1), dtype=np.uint8)
 
     for video_id, s, e, label in windows:
