059c90f3e694293ada8050f5af946b4709360e4f
amoshyc <amoshuangyc@gmail.com>
2017-07-20 15:09:38 +0800

    Update at 2017-07-20 15-09-38

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 import pathlib
 import numpy as np
-import skvideo.io
-from moviepy.editor import VideoFileClip
 
 path = pathlib.Path('~/dataset/video00/video.mp4').expanduser()
-# videodata = skvideo.io.vread(str(path))
-# _ = input(':')
-# print(videodata.shape)
 
-clip = VideoFileClip(str(path))
-frames = np.array(list(clip.iter_frames()))
-_ = input(':')
-print(frames.shape)
+# import skvideo.io
+# # videodata = skvideo.io.vread(str(path))
+# # _ = input(':')
+# # print(videodata.shape)
+
+# from moviepy.editor import VideoFileClip
+# clip = VideoFileClip(str(path))
+# frames = np.array(list(clip.iter_frames()))
+# _ = input(':')
+# print(frames.shape)
