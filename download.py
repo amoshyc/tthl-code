bb4819339472229905077f7d93ecd57fbe8b047c
amoshyc <amoshuangyc@gmail.com>
2017-07-20 16:44:03 +0800

    Update at 2017-07-20 16-44-03

--- download.py
+++ download.py
@@ -4,8 +4,11 @@ from subprocess import run
 
 dataset = Path('~/tthl-dataset/').expanduser()
 video_dirs = sorted(dataset.glob('video*/'))
+
+# run(['rm', '-rf', '{}/**/*.mp4'.format(dataset)])
+
 for i, video_dir in enumerate(video_dirs):
     print('{} ({} / {})'.format(video_dir, i, len(video_dirs)))
     url = json.load((video_dir / 'info.json').open())['video_src']
     run(['youtube-dl', '--no-playlist', '-f', '18', '-o', str(video_dir / 'video.mp4'), url])
-    print('-' * 50)
+    print('-' * 60)
