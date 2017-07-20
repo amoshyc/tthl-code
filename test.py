5ecd6f12cb26b70fb65aa97b318f6587af49a108
amoshyc <amoshuangyc@gmail.com>
2017-07-20 15:05:33 +0800

    Update at 2017-07-20 15-05-33

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import pathlib
 import skvideo.io
 path = pathlib.Path('~/dataset/video00/video.mp4').expanduser()
 videodata = skvideo.io.vread(str(path))
-_ = input()
+_ = input(':')
 print(videodata.shape)
