332d6364cf239d4b80cced06ba28849a063a050c
amoshyc <amoshuangyc@gmail.com>
2017-07-23 19:25:44 +0800

    Update at 2017-07-23 19-25-44

--- data.py
+++ data.py
@@ -141,7 +141,7 @@ def main():
     # gen.fit(video_dirs)
 
     gen = WindowNpzCreator(
-        n_train=10000, n_val=2000, fps=3, timesteps=6, overlap=3)
+        n_train=10000, n_val=2000, fps=3, timesteps=6, overlap=4)
     gen.fit(video_dirs)
 
 
