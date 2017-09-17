a8f8c3b6fe42cbf31d22a5513344973891936b26
amoshyc <amoshuangyc@gmail.com>
2017-09-17 13:13:23 +0800

    Update at 2017-09-17 13-13-23

---
+++ myutils.py
@@ -0,0 +1,50 @@
+import subprocess
+import pathlib
+from datetime import datetime
+
+from keras.callbacks import Callback, ModelCheckpoint, CSVLogger
+
+
+def video_concat_segments(infile, outfile, ss, ee):
+    assert len(ss) == len(ee), 'number of ss != number of ee'
+    for s, e in zip(ss, ee):
+        assert s <= e, f'start({s}) should be <= end({e})'
+
+    # ffmpeg -i test.mp4
+    # -filter_complex "\
+    #   [0]trim=1:10,setpts=PTS-STARTPTS[v0]; \
+    #   [0]trim=10:20,setpts=PTS-STARTPTS[v1]; \
+    #   [v0][v1]concat=n=2[out]"
+    # -map [out] out.mp4
+
+    n = len(ss)
+    inputs = [
+        f'[0]trim={ss[i]}:{ee[i]},setpts=PTS-STARTPTS[v{i}]' for i in range(n)
+    ]
+    streams = ''.join([f'[v{i}]' for i in range(n)])
+    concat = f'concat=n={n}[out]'
+    filter_cmd = ';'.join([*inputs, streams + concat])
+
+    cmd = f'ffmpeg -i {infile} -filter_complex "{filter_cmd}" -map [out] {outfile} -y'
+    subprocess.run(cmd, shell=True)
+
+
+def video_write_frames(infile, outfile='./frames/%05d.jpg', fps=1):
+    # ffmpeg -i out.mp4 -vf fps="fps=1" outfile
+    pathlib.Path(outfile).parent.mkdir(parents=True, exist_ok=True)
+    cmd = f'ffmpeg -i {infile} -vf fps="fps=1" {outfile}'
+    subprocess.run(cmd, shell=True)
+
+
+def video_get_duration(video):
+    cmd = f'ffprobe -i {video} -show_entries format=duration -v quiet -of csv="p=0"'
+    out = subprocess.check_output(cmd, shell=True)
+    return float(str(out, 'utf-8'))
+
+
+def get_callbacks(name):
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    csv_path = 'inc/cnn_' + now + '.csv'
+    ckpt_path = 'inc/cnn_' + now + '_{epoch:02d}_{val_binary_accuracy:.3f}.h5'
+
+    return [CSVLogger(csv_path), ModelCheckpoint(ckpt_path)]
---
+++ preprocess.py
@@ -0,0 +1,97 @@
+import json
+import argparse
+import subprocess
+import shutil
+from pathlib import Path
+from myutils import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ds', help='dataset', type=str, default='~/ds/')
+args = parser.parse_args()
+
+dataset = Path(args.ds).expanduser()
+video_dirs = sorted(dataset.glob('video*/'))
+
+
+def download_and_write_frames():
+    target_hl_dir = Path('./data/hl/')
+    target_hl_dir.mkdir(parents=True, exist_ok=True)
+    target_non_dir = Path('./data/non/')
+    target_non_dir.mkdir(parents=True, exist_ok=True)
+
+    for video_id, video_dir in enumerate(video_dirs):
+        # download
+        video = video_dir / 'video.mp4'
+        info = json.load((video_dir / 'info.json').open())
+        url, starts, ends = info['video_src'], info['starts'], info['ends']
+        download_cmd = f'youtube-dl --no-playlist -f 18 -o {video} {url}'
+        subprocess.run(download_cmd, shell=True)
+
+        # highlight video
+        hl_video_path = target_hl_dir / f'{video_id:02d}.mp4'
+        hl_frame_fmt = target_hl_dir / f'{video_id:02d}_%05d.jpg'
+        video_concat_segments(video, hl_video_path, starts, ends)
+        video_write_frames(hl_video_path, hl_frame_fmt, fps=2)
+
+        # non-highlight video
+        ss = [0] + info['ends']
+        es = info['starts'] + [video_get_duration(video)]
+        ss = [t + 0.25 for t in ss]
+        es = [t - 0.25 for t in es]
+        non_video_path = target_non_dir / f'{video_id:02d}.mp4'
+        non_frame_fmt = target_non_dir / f'{video_id:02d}_%05d.jpg'
+        video_concat_segments(video, non_video_path, ss, es)
+        video_write_frames(non_video_path, non_frame_fmt, fps=2)
+
+
+def train_val_split():
+    hl_dir = Path('./data/hl/')
+    non_dir = Path('./data/non/')
+
+    # yapf: disable
+    target_dirs = [
+        Path('./train/hl/'),
+        Path('./train/non/'),
+        Path('./val/hl/'),
+        Path('./val/non/')
+    ]
+    source_imgs = [
+        [x for x in hl_dir.iterdir() if int(x.stem[:2]) < 9],
+        [x for x in non_dir.iterdir() if int(x.stem[:2]) < 9],
+        [x for x in hl_dir.iterdir() if int(x.stem[:2]) >= 9],
+        [x for x in non_dir.iterdir() if int(x.stem[:2]) >= 9]
+    ] # yapf: enable
+
+    for folder, imgs in zip(target_dirs, source_imgs):
+        folder.mkdir(parents=True, exist_ok=True)
+        for img in tqdm(list(imgs), ascii=True):
+            shutil.copy(str(img), str(folder))
+
+
+def gen_npz():
+    npz_dir = Path('./npz')
+    npz_dir.mkdir(exist_ok=True)
+
+    for folder in [Path('./train/'), Path('./val/')]:
+        imgs = sorted(list(folder.glob('*/*.jpg')))
+        random.shuffle(imgs)
+        classes = list([x for x in folder.iterdir() if x.is_dir()])
+        classes.sort()
+
+        n_samples = len(imgs)
+        xs = np.zeros((n_samples, 224, 224, 3), dtype=np.float32)
+        ys = np.zeros((n_samples, ), dtype=np.uint8)
+        for i, img_path in enumerate(tqdm(imgs, ascii=True)):
+            img = Image.open(img_path)
+            img = img.resize(target_size)
+            xs[i] = np.array(img)
+            ys[i] = classes.index(img_path.parent)
+
+        npz_path = npz_dir / f'{folder.stem}.npz'
+        np.savez(npz_dir / folder.stem, xs=xs, ys=ys)
+
+
+if __name__ == '__main__':
+    download_and_write_frames()
+    train_val_split()
+    gen_npz()
